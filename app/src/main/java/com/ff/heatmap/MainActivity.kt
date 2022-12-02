c9b4ef48deac00dcf7856f1823df1bc1d520c641
lsyx <lsyx@null>
2022-12-02 19:00:10 +0800

    123

--- app/src/main/java/com/ff/heatmap/MainActivity.kt
+++ app/src/main/java/com/ff/heatmap/MainActivity.kt
@@ -1,95 +1,72 @@
-package com.ff.heatmap;
+package com.ff.heatmap
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.ImageView;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ff.heatmap.heatmap.HeatMap
+import com.ff.heatmap.heatmap.HeatMapOverlay
+import com.google.android.material.snackbar.Snackbar
+import com.ff.heatmap.heatmap.WeightedLatLng
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.ImageView
+import java.util.ArrayList
 
-import com.ff.heatmap.heatmap.HeatMap;
-import com.ff.heatmap.heatmap.HeatMapOverlay;
-import com.ff.heatmap.heatmap.WeightedLatLng;
+class MainActivity : AppCompatActivity() {
+    private var screenWidth = 0
+    private var screenHeight = 0
+    private var imageView: ImageView? = null
+    private var heatMap: HeatMap? = null
+    private var heatMapOverlay: HeatMapOverlay? = null
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-
-    private int screenWidth;
-    private int screenHeight;
-    private ImageView imageView;
-    private HeatMap heatMap;
-    private HeatMapOverlay heatMapOverlay;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        imageView = (ImageView) findViewById(R.id.image);
-        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
-        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
-
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("HeatMap");
-
-        fab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "regenerate heatmap data", Snackbar.LENGTH_LONG)
-                        .setAction("ok", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                List<WeightedLatLng> data = generateHeatMapData();
-                                heatMap.setWeightedData(data);
-                                imageView.setImageBitmap(heatMap.generateMap());
-                            }
-                        }).show();
-            }
-        });
-
-        fab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Regenerate HeatMapOverlay Data", Snackbar.LENGTH_LONG)
-                        .setAction("ok", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                List<WeightedLatLng> data = generateHeatMapData();
-                                heatMapOverlay.setWeightedData(data);
-                                imageView.setImageBitmap(heatMapOverlay.generateMap());
-                            }
-                        }).show();
-            }
-        });
-
-
-        measureScreen();
-        List<WeightedLatLng> data = generateHeatMapData();
-        heatMap = new HeatMap.Builder().weightedData(data).radius(35).width(screenWidth).height(screenHeight).build();
-        heatMapOverlay = new HeatMapOverlay.Builder().weightedData(data).radius(35).width(screenWidth).height(screenHeight).build();
-        imageView.setImageBitmap(heatMap.generateMap());
-    }
-
-    private void measureScreen() {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        screenWidth = displayMetrics.widthPixels;
-        screenHeight = displayMetrics.heightPixels;
-    }
-
-    @NonNull
-    private List<WeightedLatLng> generateHeatMapData() {
-        List<WeightedLatLng> data = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            data.add(new WeightedLatLng((int) (Math.random() * screenWidth),
-                    (int) (Math.random() * screenHeight),
-                    Math.random() * 100));
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.image)
+        val fab1 = findViewById<View>(R.id.fab1)
+        val fab2 = findViewById<View>(R.id.fab2)
+        fab1.setOnClickListener { view ->
+            Snackbar.make(view, "regenerate heatmap data", Snackbar.LENGTH_LONG)
+                .setAction("ok") {
+                    val data = generateHeatMapData()
+                    heatMap!!.setWeightedData(data)
+                    imageView!!.setImageBitmap(heatMap!!.generateMap())
+                }.show()
         }
-        return data;
+        fab2.setOnClickListener { view ->
+            Snackbar.make(view, "Regenerate HeatMapOverlay Data", Snackbar.LENGTH_LONG)
+                .setAction("ok") {
+                    val data = generateHeatMapData()
+                    heatMapOverlay!!.setWeightedData(data)
+                    imageView!!.setImageBitmap(heatMapOverlay!!.generateMap())
+                }.show()
+        }
+        measureScreen()
+        val data = generateHeatMapData()
+        heatMap =
+            HeatMap.Builder().weightedData(data).radius(35).width(screenWidth).height(screenHeight)
+                .build()
+        heatMapOverlay = HeatMapOverlay.Builder().weightedData(data).radius(35).width(screenWidth)
+            .height(screenHeight).build()
+        imageView!!.setImageBitmap(heatMap?.generateMap())
+    }
+
+    private fun measureScreen() {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        screenWidth = displayMetrics.widthPixels
+        screenHeight = displayMetrics.heightPixels
+    }
+
+    private fun generateHeatMapData(): List<WeightedLatLng> {
+        val data: MutableList<WeightedLatLng> = ArrayList()
+        for (i in 0..999) {
+            data.add(
+                WeightedLatLng(
+                    (Math.random() * screenWidth).toInt(), (Math.random() * screenHeight).toInt(),
+                    Math.random() * 100
+                )
+            )
+        }
+        return data
     }
 }
